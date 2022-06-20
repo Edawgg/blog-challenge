98a749fe82430a3d820fb4cbc5a1cba741c6aa6b
Edawgg <99540833+Edawgg@users.noreply.github.com>
2022-06-19 23:00:30 -0500

    setup

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // Import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
@@ -17,3 +18,23 @@ if (process.env.JAWSDB_URL) {
 }
 
 module.exports = sequelize;
+=======
+const Sequelize = require("sequelize")
+
+    require("dontenv").config()
+
+    const sequelize = process.env.JAWSDB_URL
+        ? new Sequelize(process.env.JAWSDB_URL)
+        : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+            host: "localhost",
+            dialect: "mysql",
+            port: 3306,
+        })
+
+        sequelize
+        .authenticate()
+        .then(() => console.log('Successfully connected to database...'))
+        .catch(err => console.log('Error connecting to the database... ${err}'))
+
+        module.exports = sequelize
+>>>>>>> Stashed changes
